1fa78a3f70eae9ad605c59d3fa822e7f235132c3
alex-coch <a@deonop.com>
2022-09-08 16:50:09 +0400

    Flask release

--- boston_housing/app.py
+++ boston_housing/app.py
@@ -1,23 +1,42 @@
-from flask import Flask
 import pickle
 
+import numpy as np
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, IntegerField
+from wtforms.validators import DataRequired, URL, NumberRange
 
 application = Flask(__name__)
+application.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap(application)
 
 
-@application.route("/")
+class MForm(FlaskForm):
+    data1 = IntegerField("'RM' is the average number of rooms among homes in the neighborhood (Ex 5)",
+                         validators=[DataRequired(), NumberRange(min=1)])
+    data2 = IntegerField("'LSTAT' is the percentage of homeowners in the neighborhood considered 'lower class' (Ex 17)",
+                         [DataRequired(), NumberRange(min=1)])
+    data3 = IntegerField(
+        "'PTRATIO' is the ratio of students to teachers in primary and secondary schools in the neighborhood (Ex 15)",
+        [DataRequired(), NumberRange(min=1)])
+    submit = SubmitField('Submit')
+
+
+@application.route("/", methods=["GET", "POST"])
 def index():
-    with open('boston_housing_model.pkl', 'rb') as f:
-        reg = pickle.load(f)
-    # Produce a matrix for client data
-    client_data = [[5, 17, 15],  # Client 1
-                   [4, 32, 22],  # Client 2
-                   [8, 3, 12]]   # Client 3
+    form = MForm()
     ret = ""
-    for i, price in enumerate(reg.predict(client_data)):
-        ret += "Predicted selling price for Client {ii}'s home: ${pp:.2f}<br>".format(ii = i+1, pp = price)
-    return ret
+    if form.validate_on_submit():
+        with open('boston_housing_model.pkl', 'rb') as f:
+            reg = pickle.load(f)
+        client_data = np.array([form.data1.data, form.data2.data, form.data3.data]).reshape(1, -1)
+        price = reg.predict(client_data)
+        ret = "Predicted selling price for the home: ${pp:.2f}".format(pp=float(price[0]))
+
+    return render_template('index.html', form=form, ret=ret)
 
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0', port='8080')
+    # application.run(debug='on')
